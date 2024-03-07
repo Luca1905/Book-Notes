9ffdc1c003d91287da12c06492e92259bfe5f4e4
Luca1905 <wluca2008@gmail.com>
2024-03-07 17:32:53 +0100

    switched to database

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
+import fs from "fs";
+import pg from "pg";
 import express from "express";
 import bodyParser from "body-parser";
-import path, {dirname} from "path";
-import {fileURLToPath} from "url";
+import axios from "axios";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Book Notes",
+  password: "123456",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
 
-const notes = [
-    {
-        id: 0,
-        title: "Book 1",
-        author: "Author 1",
-        rating: 5,
-        coverImage: "images/book1.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel nisi semper, interdum felis eget, pulvinar sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce pellentesque placerat semper. Fusce pulvinar tortor ac mauris faucibus laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.",
-    },
-    {
-        id: 1,
-        title: "Book 2",
-        author: "Author 2",
-        rating: 4,
-        coverImage: "images/book2.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel nisi semper, interdum felis eget, pulvinar sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce pellentesque placerat semper. Fusce pulvinar tortor ac mauris faucibus laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.",
-    }
-]
+db.connect();
 
 // Set view engine and views directory
 app.set("view engine", "ejs");
@@ -33,14 +27,40 @@ app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.get("/", (req, res) => {
-    res.render("index", {notes: notes});
+app.get("/", async (req, res) => {
+  const notes = await getNotesFromDatabase();
+  try {
+    for (let note = 0; note < notes.length; note++) {
+      if (
+        !fs.existsSync(
+          path.join(__dirname, "/public/images", `${notes[note].id}.jpg`)
+        )
+      ) {
+        const response = await axios.get(
+          `https://covers.openlibrary.org/b/isbn/${notes[note].isbn}-L.jpg?default=false`,
+          { responseType: "arraybuffer" }
+        );
+        console.log(notes[note].id + "succesful");
+        fs.writeFileSync(
+          path.join(__dirname, "/public/images", `${notes[note].id}.jpg`),
+          Buffer.from(response.data, "binary")
+        );
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.render("index", { notes: notes });
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
+async function getNotesFromDatabase() {
+  const { rows: notes } = await db.query('SELECT * FROM notes');
+  return notes;}
